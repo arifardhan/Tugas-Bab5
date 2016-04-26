ea41c1ce70927a3d7a0fb768ff5a6e54e9bc3985
arifardhan <ariframadhan902@yahoo.com>
2016-04-26 20:36:38 +0700

    Membuat pilihan menu

--- src/tugas/MainMenghitung.java
+++ src/tugas/MainMenghitung.java
@@ -1,5 +1,24 @@
 package Praktikum5;
 
+import java.util.Scanner;
+
 public class MainMenghitung {
-    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int pilih = 0;
+        while (pilih != 6) {
+            System.out.println("++++    Menu    ++++");
+            System.out.println("(1) Penjumlahan");
+            System.out.println("(2) Pengurangan");
+            System.out.println("(3) Perkalian");
+            System.out.println("(4) pembagian");
+            System.out.println("(5) Penyederhanaan");
+            System.out.println("(6) Keluar");
+            System.out.println("===================");
+            System.out.print("Masukkan pilihan");
+            pilih = in.nextInt();
+            
+        }
+    }
 }
