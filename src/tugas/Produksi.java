af29c7b988f171b18f3c6e65b3a51df5a3247dbd
arifardhan <ariframadhan902@yahoo.com>
2016-04-26 21:59:02 +0700

    Menyelesaikan method di class

--- src/tugas/Produksi.java
+++ src/tugas/Produksi.java
@@ -9,4 +9,25 @@ public class Produksi {
     public int harga[] = new int[3];
     public int bayar;
 
+    public void setBeli(int a[]) {
+        jumlah = a;
+        bayar = 0;
+        for (int i = 0; i < 3; i++) {
+            if (a[i] > 100) {
+                harga[i] = (hargajaket[i] * a[i]) - (diskon[i] * a[i]);
+            } else {
+                harga[i] = (hargajaket[i] * a[i]);
+            }
+            bayar += harga[i];
+        }
+    }
+
+    public void cetakHarga() {
+        System.out.println("Nama    Jumlah    Harga");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(produk[i] + "    " + jumlah[i] + "      Rp." + harga[i]);
+        }
+        System.out.println("=======================================");
+        System.out.println("Total Harga: Rp." + bayar);
+    }
 }
