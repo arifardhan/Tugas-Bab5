ab1d75720e4545d619f7936e053b7e007296bc0b
arifardhan <ariframadhan902@yahoo.com>
2016-04-26 21:57:39 +0700

    Membuat inputan pembelian jaket

--- src/tugas/Mainproduksi.java
+++ src/tugas/Mainproduksi.java
@@ -1,5 +1,21 @@
 package Praktikum5;
 
+import java.util.Scanner;
+
 public class Mainproduksi {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+         Produksi jaket = new Produksi();
+         int a[] = new int[3];
+         System.out.println("======CV.Labkomdas======");
+         for (int i = 0; i < 3; i++) {
+             System.out.print("Masukkan Jumlah "+jaket.produk[i]+" : ");
+             a[i]=in.nextInt();
+         }
+         jaket.setBeli(a);
+         System.out.println("========================================");
+         jaket.cetakHarga();
+
+    }
 }
