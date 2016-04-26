a9055012defc7f3a56c948cd16c53045e10c35f4
arifardhan <ariframadhan902@yahoo.com>
2016-04-26 20:56:13 +0700

    Membuat case dari pilihan menu

--- src/tugas/MainMenghitung.java
+++ src/tugas/MainMenghitung.java
@@ -16,9 +16,69 @@ public class MainMenghitung {
             System.out.println("(5) Penyederhanaan");
             System.out.println("(6) Keluar");
             System.out.println("===================");
-            System.out.print("Masukkan pilihan");
+            System.out.print("Masukkan pilihan  : ");
             pilih = in.nextInt();
-            
+            Menghitung hitung = new Menghitung();
+            switch (pilih) {
+                case 1:
+                    System.out.println();
+                    System.out.println("+PENJUMLAHAN+");
+                    System.out.print("masukkan nilai 1 : ");
+                    int nil1 = in.nextInt();
+                    System.out.print("masukkan nilai 2 : ");
+                    int nil2 = in.nextInt();
+                    Menghitung.penjumlahan(nil1, nil2);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println();
+                    System.out.println("-PENGURANGAN-");
+                    System.out.print("masukkan nilai 1 : ");
+                    int nilai1 = in.nextInt();
+                    System.out.print("masukkan nilai 2 : ");
+                    int nilai2 = in.nextInt();
+                    Menghitung.pengurangan(nilai1, nilai2);
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println();
+                    System.out.println("x PERKALIAN x");
+                    System.out.print("masukkan nilai 1 : ");
+                    int nilaia = in.nextInt();
+                    System.out.print("masukkan nilai 2 : ");
+                    int nilaib = in.nextInt();
+                    hitung.perkalian(nilaia, nilaib);
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println();
+                    System.out.println(": PEMBAGIAN :");
+                    System.out.print("masukkan nilai 1 : ");
+                    int nilaix = in.nextInt();
+                    System.out.print("masukkan nilai 2 : ");
+                    int nilaiy = in.nextInt();
+                    hitung.pembagian(nilaix, nilaiy);
+                    System.out.println();
+                    break;
+                case 5:
+                    System.out.println();
+                    System.out.println("/ PENYEDERHANAAN /");
+                    System.out.print("masukkan nilai 1 : ");
+                    int a = in.nextInt();
+                    System.out.print("masukkan nilai 2 : ");
+                    int b = in.nextInt();
+                    hitung.sederhana(a, b);
+                    System.out.println();
+                    break;
+                case 6:
+                    System.out.println();
+                    System.out.println("Terimakasih");
+                    break;
+                default: 
+                    System.out.println();
+                    System.out.println("Maaf menu tidak terdaftar...");
+                    System.out.println();
+            }
         }
     }
 }
