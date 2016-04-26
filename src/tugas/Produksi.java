46e264131d8699d11f18888106b0066c901cb86c
arifardhan <ariframadhan902@yahoo.com>
2016-04-26 21:45:47 +0700

    Membuat atribut class

--- src/tugas/Produksi.java
+++ src/tugas/Produksi.java
@@ -2,4 +2,11 @@ package Praktikum5;
 
 public class Produksi {
 
+    public String produk[] = {"Jaket A", "Jaket B", "Jaket C"};
+    public final int hargajaket[] = {100000, 125000, 175000};
+    public int jumlah[];
+    public int diskon[] = {5000, 5000, 15000};
+    public int harga[] = new int[3];
+    public int bayar;
+
 }
